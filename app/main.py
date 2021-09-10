90690f71400b59536661dcc7b2e4e9a1e8237a0f
梁 <1227689971@qq.com>
2021-09-11 00:03:35 +0800

    fix app.run(debug=True) to app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ def hello_world(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8999)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
